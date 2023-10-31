588eccc235d68afcec7a333bae36885c3dc36f58
newman174 <51267123+newman174@users.noreply.github.com>
2023-10-31 11:47:50 -0400

    cleanup

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,14 +1,8 @@
 import express from "express";
 import ticketsRouter from "./api/tickets";
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
 
 const router = express.Router();
 
 router.use("/api/tickets", ticketsRouter);
-
-// export const srcDir = dirname(fileURLToPath(import.meta.url));
-
-// router.use(express.static(srcDir + "/../dist"));
 
 export default router;
